0174dfc40265d78074f9aba4ea590c43e4d05698
Karamjeet Singh <janduk@student.douglascollege.ca>
2022-09-19 09:39:57 -0700

    Basics verified

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import TestBasics from "./TestBasics";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App title="Hey React Dev tools" />);
+root.render(<TestBasics />);
